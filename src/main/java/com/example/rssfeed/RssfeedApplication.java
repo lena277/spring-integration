99583f5c56f7e9c35eb73bdb97dd8ae585ee9bd7
lena277 <lenaasfour277@gmail.com>
2019-05-09 17:11:02 +0300

    Second commit - upadated file - RssFeedApplication

--- src/main/java/com/example/rssfeed/RssfeedApplication.java
+++ src/main/java/com/example/rssfeed/RssfeedApplication.java
@@ -1,52 +1,12 @@
 package com.example.rssfeed;
 
-import com.rometools.rome.feed.synd.SyndEntry;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
-import org.springframework.core.task.TaskExecutor;
-import org.springframework.integration.annotation.InboundChannelAdapter;
-import org.springframework.integration.annotation.Poller;
-import org.springframework.integration.dsl.IntegrationFlow;
-import org.springframework.integration.dsl.IntegrationFlows;
-import org.springframework.integration.dsl.MessageChannels;
-import org.springframework.integration.dsl.Pollers;
-import org.springframework.integration.feed.dsl.Feed;
-import org.springframework.integration.file.FileReadingMessageSource;
-import org.springframework.integration.file.FileWritingMessageHandler;
-import org.springframework.integration.file.dsl.Files;
-import org.springframework.integration.file.filters.SimplePatternFileListFilter;
-import org.springframework.integration.file.support.FileExistsMode;
-import org.springframework.integration.handler.LoggingHandler;
-import org.springframework.integration.metadata.MetadataStore;
-import org.springframework.integration.metadata.PropertiesPersistingMetadataStore;
-import org.springframework.integration.splitter.AbstractMessageSplitter;
-import org.springframework.integration.transformer.AbstractPayloadTransformer;
-import org.springframework.integration.xml.splitter.XPathMessageSplitter;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.MessageHandler;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-
-
-import java.io.File;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.concurrent.Executor;
 
 @SpringBootApplication
-@Configuration
 public class RssfeedApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RssfeedApplication.class, args);
     }
-
-
 }
